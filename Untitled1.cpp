14b1eac71783493d46d4581c56c52a6bb6b7d648
Jela Antoinette Jeminez <jelajeminez8@gmail.com>
2024-08-24 23:47:19 +0800

    Add files via upload
    I create a code for display to a menu with options for the user to. chars

---
+++ Untitled1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+ 
+using namespace std;
+	
+int main(){
+	
+	int choice;
+		do
+{
+		cout << endl
+		<< " List files in the current directory\n"
+		<< " Create a new directory\n"
+		<< " Change the working directory\n"
+		<< " Exit the program.\n"
+		<< " Enter your choice and press return: ";
+		cin >> choice;
+
+			return 0;
+}
